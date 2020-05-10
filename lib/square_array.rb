b67dbe51af5b5189664a6b033a82c556c8d0479a
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-10 21:42:16 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ array = [1,2,3,4]
 def square_array(array)
   new_array = []
   count = 0
-  squared = new_array.push(array**)
+  squared = new_array.push(array[count]**)
   
   while count < array.length do
     puts squared
