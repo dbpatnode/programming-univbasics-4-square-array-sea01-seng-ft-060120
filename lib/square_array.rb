1c9f5e6c9d62db85e1e0bd3342a7ed9a34d15c05
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-10 21:53:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,9 +2,10 @@ array = [1,2,3,4]
 def square_array(array)
   new_array = []
   count = 0
-  new_array.push(array[count]**2)
+ 
   
   while count < array.length do
+    
     count += 1
   end
   return new_array
