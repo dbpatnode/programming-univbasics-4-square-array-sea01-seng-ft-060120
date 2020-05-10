38940f32ebd833118304f21169b53a31787b6d9a
Daniel Patnode <dbpatnode@hotmail.com>
2020-05-10 20:52:57 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
-array = [1,2,3,4]
+numbers = [1,2,3,4]
 
 def square_array(array)
-counter = 0
-  while array[counter] do
-  puts array[counter]**
+
+  while square_array(numbers) do
+  
   counter +=
   end
 end
